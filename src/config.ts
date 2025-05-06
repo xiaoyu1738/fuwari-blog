78128009a1d28b8055dba906ad936106decec641
xiaoyu1738 <xiaoyu1738jw@gmail.com>
2025-05-06 23:34:53 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/xiaoyu1738", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
